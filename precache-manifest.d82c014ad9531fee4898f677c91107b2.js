806d3815c541a4dd14275518a1d396f6c4d2bb28
NKwatra <kwatranishkarsh@gmail.com>
2019-11-16 20:27:40 +0530

    Updates

---
+++ precache-manifest.d82c014ad9531fee4898f677c91107b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9d9a9ce68e5d0a7505dc85074607b9",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "506c617f01e8ba92353b",
+    "url": "/TodoList/static/css/main.09391488.chunk.css"
+  },
+  {
+    "revision": "5176104b899645c4c193",
+    "url": "/TodoList/static/js/2.969761f2.chunk.js"
+  },
+  {
+    "revision": "506c617f01e8ba92353b",
+    "url": "/TodoList/static/js/main.5ffa8d43.chunk.js"
+  },
+  {
+    "revision": "9c8a38fc2c158de22897",
+    "url": "/TodoList/static/js/runtime-main.cca56c41.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/TodoList/static/media/logo.25bf045c.svg"
+  }
+]);
